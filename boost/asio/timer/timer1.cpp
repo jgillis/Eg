efec71a8b33d30a1ed6b4e07e369093d3af53217
JG <joris.gillis@mech.kuleuven.be>
2011-10-24 00:10:46 +0200

    Added boost::asio

---
+++ boost/asio/timer/timer1.cpp
@@ -0,0 +1,26 @@
+// http://www.boost.org/doc/libs/1_44_0/doc/html/boost_asio/tutorial/tuttimer1/src.html
+// timer.cpp
+// ~~~~~~~~~
+//
+// Copyright (c) 2003-2010 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// 
+//
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+int main()
+{
+  boost::asio::io_service io;
+
+  boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+  t.wait();
+
+  std::cout << "Hello, world!\n";
+
+  return 0;
+}
