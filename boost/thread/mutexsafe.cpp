cd2ed5fb9f290d6d8365f9ed014742f943873fac
JG <joris.gillis@mech.kuleuven.be>
2011-11-03 23:44:04 +0100

    Added boost/thread examples

---
+++ boost/thread/mutexsafe.cpp
@@ -0,0 +1,49 @@
+#include <boost/thread.hpp>
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+ 
+struct timeval time_temp;
+
+boost::mutex mm;
+
+double gettime() {
+  boost::mutex::scoped_lock  lock(mm);
+  gettimeofday(&time_temp, NULL);
+  return time_temp.tv_sec + time_temp.tv_usec/1e6;
+}
+
+void task1() { 
+    int max = 1e8;
+    int dummy=0;
+    for (int i=0;i<max;i++) {
+      double time_here = gettime();
+      while(gettime()-time_here < 5) {dummy ++;}
+      std::cout << "foo " << i << ":" << (gettime()-time_here) << std::endl;
+    };
+}
+
+void task2() { 
+    int max = 1e8;
+    int dummy=0;
+    for (int i=0;i<max;i++) {
+      double time_here = gettime();
+      while(gettime()-time_here < 1) {dummy ++;}
+      std::cout << " bar " << i << ":" << (gettime()-time_here) << std::endl;
+    };
+}
+
+int main (int argc, char ** argv) {
+    using namespace boost; 
+    thread thread_1 = thread(task1);
+    thread thread_2 = thread(task2);
+
+    // do other stuff
+    thread_2.join();
+    thread_1.join();
+    return 0;
+}
+
