8f268d59a3860ba1970e906a9bb4ba1d0cbc5f71
Joris Gillis <joris.gillis@mech.kuleuven.be>
2011-11-22 18:41:21 +0100

    mpi examples

---
+++ mpi/python/master.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from mpi4py import MPI
+import numpy
+import sys
+
+print sys.executable
+
+comm = MPI.COMM_SELF.Spawn(sys.executable,
+                           args=['slave.py'],
+                           maxprocs=5)
+
+N = numpy.array(100, 'i')
+comm.Bcast([N, MPI.INT], root=MPI.ROOT)
+PI = numpy.array(0.0, 'd')
+comm.Reduce(None, [PI, MPI.DOUBLE],
+            op=MPI.SUM, root=MPI.ROOT)
+print(PI)
+
+comm.Disconnect()
