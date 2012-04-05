e1c75789b914819d6eae2af6450ad6b18c098217
Joris <joris.gillis@mech.kuleuven.be>
2012-04-05 17:42:45 +0200

    more with buildbot

--- buildbot/run.sh
+++ buildbot/run.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+rm -rf dummy dummy_rep master slave
+
+echo "Creating a dummy project"
+mkdir -p dummy.git && cd dummy.git && git --bare init   && cd ..
+git clone dummy.git dummy
+
 buildbot create-master -r master
-cp master/master.cfg.sample master/master.cfg
+cp master.cfg master/master.cfg
+
+echo "tweak the settings a bit"
+
 buildslave create-slave slave localhost:9989 example-slave pass
 
+cd master && buildbot checkconfig master.cfg
+cd ..
+echo `pwd`
 cd master && buildbot start && cd ..
 cd slave && buildslave start && cd ..
 
+
+
+firefox http://localhost:8010/
+
+echo "You can go and login with dummy dummy"
+
+cd dummy && echo "buildbot is awesome" > test.txt && git add test.txt && git commit -m "first commit" && git push origin master
+echo "so far" >> test.txt && git add test.txt && git commit -m "second commit" && git push origin master && cd ..
