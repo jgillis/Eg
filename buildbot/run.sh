8892b16ccaa9956bb518ec5b3a8d9dbe2cc50da8
Joris <joris.gillis@mech.kuleuven.be>
2012-04-11 16:20:53 +0200

    buildbot: debug

--- buildbot/run.sh
+++ buildbot/run.sh
@@ -15,6 +15,9 @@ buildslave create-slave slave localhost:9989 example-slave pass
 cd master && buildbot checkconfig master.cfg
 cd ..
 echo `pwd`
+
+cat ~/.ssh/id_rsa.pub > master/authorized_keys
+
 cd master && buildbot start && cd ..
 cd slave && buildslave start && cd ..
 
@@ -23,6 +26,8 @@ cd slave && buildslave start && cd ..
 firefox http://localhost:8010/
 
 echo "You can go and login with dummy dummy"
+
+echo "You can debug over ipython-ssh with: ssh -p 7812 jg@localhost"
 
 cd dummy && echo "buildbot is awesome" > test.txt && git add test.txt && git commit -m "first commit" && git push origin master
 echo "so far" >> test.txt && git add test.txt && git commit -m "second commit" && git push origin master && cd ..
